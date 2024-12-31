85b3aa2dd76b503bcf586cfba98af6bfdda02601
Pankaj Sharma <pankaj.avology@gmail.com>
2024-12-31 18:17:58 +0530

    Updated ui

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+// slick slider code
+$('.perfectSlider').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1,
+    autoplay: true,
+    arrows: true,
+    dots: true,
+    speed: 500,
+    autoplaySpeed: 1000000,
+    
+  });
